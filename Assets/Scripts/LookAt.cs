5a5bc91bfeb04a663d891ca5bf477937486137d3
Joakim Linna <themanzed@gmail.com>
2022-03-16 11:53:58 +0100

    Vr commanding (#4)
    * created scene for vr commanding
    
    * quest build settings
    
    * sdk apk path fix
    
    * fixed conflicts
    
    * project settings
    
    not really any changes made?
    
    * Fetched and builded
    
    No changes only build apk after last fetch
    
    * project settings?
    
    * fixed the scene and building issues
    
    Co-authored-by: kenny strand <kenny.strand@ga.klaragymnasium.se>

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -1,19 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAt : MonoBehaviour
 {
-    public GameObject Camera;
-    // Start is called before the first frame update
-    void Start()
+    private Camera _camera;
+
+    private void Awake()
     {
-        
+        _camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.transform);
+        transform.LookAt(_camera.transform);
     }
 }
