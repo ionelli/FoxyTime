b6eb91d8b1fee4fc9af4372f94c493faa4c928f1
AntonJanLindholm <66430940+AntonJanLindholm@users.noreply.github.com>
2022-03-18 12:48:13 +0100

    Audio assets (#5)
    * Background sounds
    
    Chill background music + randomised birds chippering, jungle sounds and wind sounds appearing every 30 second.
    
    * updateljudigen
    
    .-
    
    * Revert "Background sounds"
    
    This reverts commit 4e6e55119ea79670959adcbeb8c90f78a1d849d0.
    
    * test
    
    test
    
    * Background done.
    
    Background done.
    
    * Footsteps implimented
    
    Footsteps implimented

---
+++ Assets/Scripts/Audioscript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audioscript : MonoBehaviour
+{
+    public AudioClip[] sounds;
+    private AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Playrandom",30,30);
+    }
+
+void Playrandom(){
+
+    source = GetComponent<AudioSource>();
+
+    source.clip = sounds[Random.Range(0,sounds.Length)];
+    source.PlayOneShot(source.clip);
+}
+
+}
