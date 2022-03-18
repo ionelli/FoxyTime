349dc3ff05a72b3e8ab2f671268a84cf2489bd0b
Joakim Linna <themanzed@gmail.com>
2022-03-18 12:47:28 +0100

    Interactables (#6)
    * should have working interactables now
    
    * holdable items
    
    * Happy particle
    
    * playstop correctly
    
    * added return logic for interactables
    
    Co-authored-by: kenny strand <kenny.strand@ga.klaragymnasium.se>

--- Assets/HandInteraction.cs
+++ Assets/HandInteraction.cs
@@ -2,66 +2,40 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 using static OVRHand;
 
-public class HandInteraction : MonoBehaviour
+public class HandInteraction : OVRGrabber
 {
     [SerializeField, Range(0,1)] private float minPinchStrength = 0.5f;
-    [SerializeField] private float handReachDistance = 1f;
-    [SerializeField] private LayerMask holdableItemMask;
+    [SerializeField] private OVRHand _hand;
 
     private Rigidbody _heldItem;
-    private OVRHand _hand;
+    
+
+    private OVRGrabber _grabber;
 
     private bool HasItem => _heldItem != null;
 
-    private void Awake()
+    public override void Update()
     {
-        _hand = GetComponent<OVRHand>();
-    }
+        base.Update();
 
-    private void Update()
-    {
         bool pinching = IsPinching();
 
-        if (HasItem && !pinching)
-        {
-            _heldItem.isKinematic = false;
-            _heldItem.transform.SetParent(null);
-            Debug.Log($"Dropped {_heldItem.gameObject.name}");
-            _heldItem = null;
-            return;
-        }
-
-        if (!HasItem && pinching)
-        {
-            if(!_hand.IsPointerPoseValid)
-                return;
-            
-            Vector3 pointDir = _hand.PointerPose.forward;
-            Vector3 pointPos = _hand.PointerPose.position;
-
-            if (Physics.Raycast(pointPos, pointDir,
-                    out RaycastHit hit, handReachDistance, holdableItemMask))
-            {
-                if(!hit.collider.TryGetComponent(out _heldItem))
-                    return;
-                
-                _heldItem.transform.SetParent(_hand.transform);
-                _heldItem.transform.localPosition = Vector3.zero;
-                Debug.Log($"Picked up {_heldItem.gameObject.name}");
-            }
-        }
+        if (!m_grabbedObj && pinching && m_grabCandidates.Count > 0)
+            GrabBegin();
+        else if (m_grabbedObj && !pinching)
+            GrabEnd();
     }
 
     private bool IsPinching()
     {
-        bool validPinch = _hand.GetFingerIsPinching(HandFinger.Thumb) 
-                          && _hand.GetFingerIsPinching(HandFinger.Index);
+        bool validPinch = _hand.GetFingerIsPinching(HandFinger.Index);
 
         if (!validPinch)
             return false;
 
-        return !(_hand.GetFingerPinchStrength(HandFinger.Index) < minPinchStrength);
+        return _hand.GetFingerPinchStrength(HandFinger.Index) > grabBegin;
     }
 }
---
+++ Assets/ObjectReturn.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectReturn : MonoBehaviour
+{
+    [SerializeField] private float startDistanceBuffer = 1f;
+    [SerializeField] private float returnWaitTime;
+
+    private bool _hasMoved;
+    private Vector3 _startPos;
+    private float _returnWait;
+    private Rigidbody _body;
+    private OVRGrabbable _grabbable;
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody>();
+        _grabbable = GetComponent<OVRGrabbable>();
+        _startPos = transform.position;
+    }
+
+    void Update()
+    {
+        if(transform.position.y < -5f)
+            ReturnToStart();
+        
+        if (_hasMoved)
+        {
+            if (_grabbable.isGrabbed)
+            {
+                _returnWait = returnWaitTime;
+                return;
+            }
+            
+            _returnWait -= Time.deltaTime;
+            if (_returnWait <= 0)
+                ReturnToStart();
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, _startPos) < startDistanceBuffer) 
+            return;
+
+        if (_grabbable.isGrabbed)
+            return;
+        
+        _hasMoved = true;
+        _returnWait = returnWaitTime;
+    }
+
+    private void ReturnToStart()
+    {
+        transform.position = _startPos;
+        _hasMoved = false;
+        _body.velocity = Vector3.zero;
+    }
+}
